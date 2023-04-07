85d6015e2575653912cfe707cc01ee4c1745830a
rlawlgns47 <65403924+rlawlgns47@users.noreply.github.com>
2023-04-08 01:18:23 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,6 +1,9 @@
 from cmath import log
 from distutils.sysconfig import PREFIX
 import discord
+from dotenv import load_dotenv
+import os
+load_dotenv()
 from discord import Embed
 import requests
 from discord.ext import commands
