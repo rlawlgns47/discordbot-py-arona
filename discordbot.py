529a6cc5de297232995c697f85475de7aa328a4f
rlawlgns47 <65403924+rlawlgns47@users.noreply.github.com>
2024-10-02 03:03:29 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -87,6 +87,15 @@ def get_seoul_weather():
     }
 
 
+def wait_on_run(run, thread):
+    while run.status == "queued" or run.status == "in_progress":
+        run = client.beta.threads.runs.retrieve(
+            thread_id=thread.id,
+            run_id=run.id,
+        )
+        time.sleep(0.5)
+    return run
+
 @app.event
 async def on_ready():
     print('Done')
